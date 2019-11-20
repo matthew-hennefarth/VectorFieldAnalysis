df60dd32e707bdc359a049c1887d4e4bafd90363
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2019-11-19 17:23:25 -0800

    Everything seems to be working normally

--- src/Calculation.cpp
+++ src/Calculation.cpp
@@ -3,16 +3,19 @@
 //
 
 #include <iostream>
+#include <fstream>
 
 #include "Grid.h"
 #include "Calculation.h"
 
 Calculation::Calculation() {
 
-    Grid x ("efield.out");
+    Grid x("efield.out");
+    std::ofstream dataFile("2D_Histo.dat");
+
 
     //Loop over total number of points
-    for(int i = 0; i < 100000; i++){
+    for (int i = 0; i < 100000; i++) {
         auto len = x.randomLength();
         auto start = x.randomPoint();
         auto initialCurvature = x.curvature(start);
@@ -20,14 +23,14 @@ Calculation::Calculation() {
         auto prev_2 = start;
         auto prev_3 = start;
         //Bad initial guess, start again
-        if(initialCurvature == -1){
+        if (initialCurvature == -1) {
             i--;
             continue;
         }
-        for(unsigned int j = 0; j < len; j++){
+        for (unsigned int j = 0; j < len; j++) {
             auto next = x.findNextPosition(prev);
             //This means we can't make a next move!
-            if(next == std::array<int, 3>{-1 ,-1 ,-1}){
+            if (next == std::array<int, 3>{-1, -1, -1}) {
                 break;
             }
             prev_3 = prev_2;
@@ -37,10 +40,10 @@ Calculation::Calculation() {
         }
 
         auto finalCurvature = x.curvature(prev);
-        if(finalCurvature == -1){
+        if (finalCurvature == -1) {
             prev = prev_2;
             finalCurvature = x.curvature(prev);
-            if(finalCurvature == -1){
+            if (finalCurvature == -1) {
                 prev = prev_3;
                 finalCurvature = x.curvature(prev);
             }
@@ -48,5 +51,13 @@ Calculation::Calculation() {
         double averageCurvature = (finalCurvature + initialCurvature) / 2.0;
         double distance = (x.at(start)[0] - x.at(prev)[0]).norm();
         std::cout << "Distance: " << distance << "\tCurvature: " << averageCurvature << '\n';
+        if (dataFile.is_open()) {
+            dataFile << distance << ' ' << averageCurvature << '\n';
+        }
     }
+    if (dataFile.is_open()){
+        dataFile << std::flush;
+        dataFile.close();
+    }
+
 }
